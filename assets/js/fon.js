2c6e4fd00db655415c7437239734a527bf36fb06
jkoidahl <jkoidahl@lssinc.com>
2015-04-16 23:53:36 -0500

    updated slideshow

--- assets/js/fon.js
+++ assets/js/fon.js
@@ -3,6 +3,7 @@
         $('#nav').slicknav({prependTo: '#header-wrapper'});
         $('.slicknav_menu').prepend('<a class="logo" href="index.html"><img src="img/fon2015logo263137.png" height="57px" alt="Festival Of Nations"/></a>');
         $('.slideshow').backstretch([
+            '/img/FON_000.jpg',
             '/img/FON_001.jpg',
             '/img/FON_003.jpg',
             '/img/FON_002.jpg',
@@ -12,7 +13,8 @@
             '/img/FON_007.jpg',
             '/img/FON_008.jpg',
             '/img/FON_009.jpg',
-            '/img/FON_010.jpg'
+            '/img/FON_010.jpg',
+            '/img/FON_000a.jpg'
         ], {duration: 3000, fade: 750});
     } );
 } ) ( jQuery );
