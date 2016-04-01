117320d2b70be9aefc1bff9f2eb0570716fabcdc
jkoidahl <jkoidahl@lssinc.com>
2016-03-31 21:13:35 -0500

    2016 updates

--- assets/js/fon.js
+++ assets/js/fon.js
@@ -3,7 +3,7 @@
         $('#nav').slicknav({prependTo: '#header-wrapper'});
         $('.slicknav_menu').prepend('<a class="logo" href="index.html"><img src="img/fon2015logo263137.png" height="57px" alt="Festival Of Nations"/></a>');
         $('.slideshow').backstretch([
-            '/img/FON_000.jpg',
+            '/img/FON_000b.jpg',
             '/img/FON_001.jpg',
             '/img/FON_003.jpg',
             '/img/FON_002.jpg',
@@ -14,7 +14,8 @@
             '/img/FON_008.jpg',
             '/img/FON_009.jpg',
             '/img/FON_010.jpg',
-            '/img/FON_000a.jpg'
+            '/img/FON_000a.jpg',
+            '/img/FON_000.jpg'
         ], {duration: 3000, fade: 750});
     } );
 } ) ( jQuery );
